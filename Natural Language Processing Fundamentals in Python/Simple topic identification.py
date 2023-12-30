4108712558c75fd5ec77097ff80055aba3be0fff
wahyu ikbal maulana <wahyuikbal777@gmail.com>
2023-12-31 06:42:19 +0700

    Create Simple topic identification.py

---
+++ Natural Language Processing Fundamentals in Python/Simple topic identification.py
@@ -0,0 +1,14 @@
+# Import Counter
+from collections import Counter
+
+# Tokenize the article: tokens
+tokens = word_tokenize(article)
+
+# Convert the tokens into lowercase: lower_tokens
+lower_tokens = [t.lower() for t in tokens]
+
+# Create a Counter with the lowercase tokens: bow_simple
+bow_simple = Counter(lower_tokens)
+
+# Print the 10 most common tokens
+print(bow_simple.most_common(10))
