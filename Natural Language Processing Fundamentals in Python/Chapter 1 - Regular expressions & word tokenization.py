f88aa35e57f3a6e7af903df876859b40e761e9a1
wahyu ikbal maulana <wahyuikbal777@gmail.com>
2023-12-31 06:40:25 +0700

    Create  Chapter 1 - Regular expressions & word tokenization.py

---
+++ Natural Language Processing Fundamentals in Python/Chapter 1 - Regular expressions & word tokenization.py
@@ -0,0 +1,34 @@
+# Write a pattern to match sentence endings: sentence_endings
+sentence_endings = r"[.?!]"
+
+# Split my_string on sentence endings and print the result
+print(re.split(sentence_endings, my_string))
+
+# Find all capitalized words in my_string and print the result
+capitalized_words = r"[A-Z]\w+"
+print(re.findall(capitalized_words, my_string))
+
+# Split my_string on spaces and print the result
+spaces = r"\s+"
+print(re.split(spaces, my_string))
+
+# Find all digits in my_string and print the result
+digits = r"\d+"
+print(re.findall(digits, my_string))
+
+----------------------------------------
+# Import necessary modules
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+
+# Split scene_one into sentences: sentences
+sentences = sent_tokenize(scene_one)
+
+# Use word_tokenize to tokenize the fourth sentence: tokenized_sent
+tokenized_sent = word_tokenize(sentences[3])
+
+# Make a set of unique tokens in the entire scene: unique_tokens
+unique_tokens = set(word_tokenize(scene_one))
+
+# Print the unique tokens result
+print(unique_tokens)
