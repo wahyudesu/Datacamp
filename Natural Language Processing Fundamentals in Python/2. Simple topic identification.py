a9eff3282c805ead19a4626565bdc6f958fae704
wahyu ikbal maulana <wahyuikbal777@gmail.com>
2023-12-31 09:18:59 +0700

    Update 2. Simple topic identification.py

--- Natural Language Processing Fundamentals in Python/2. Simple topic identification.py
+++ Natural Language Processing Fundamentals in Python/2. Simple topic identification.py
@@ -12,3 +12,47 @@ bow_simple = Counter(lower_tokens)
 
 # Print the 10 most common tokens
 print(bow_simple.most_common(10))
+
+----------------------------------------------------------
+
+# Import Dictionary
+from gensim.corpora.dictionary import Dictionary
+
+# Create a Dictionary from the articles: dictionary
+dictionary = Dictionary(articles)
+
+# Select the id for "computer": computer_id
+computer_id = dictionary.token2id.get("computerv")
+
+# Use computer_id with the dictionary to print the word
+print(dictionary.get(computer_id))
+
+# Create a MmCorpus: corpus
+corpus = [dictionary.doc2bow(article) for article in articles]
+
+# Print the first 10 word ids with their frequency counts from the fifth document
+print(corpus[4][:10])
+
+---------------------------------------------------------
+
+# Save the fifth document: doc
+doc = corpus[4]
+
+# Sort the doc for frequency: bow_doc
+bow_doc = sorted(doc, key=lambda w: w[1], reverse=True)
+
+# Print the top 5 words of the document alongside the count
+for word_id, word_count in bow_doc[:5]:
+    print(dictionary.get(word_id), word_count)
+    
+# Create the defaultdict: total_word_count
+total_word_count = defaultdict(int)
+for word_id, word_count in itertools.chain.from_iterable(corpus):
+    total_word_count[word_id] += word_count
+
+# Create a sorted list from the defaultdict: sorted_word_count 
+sorted_word_count = sorted(total_word_count.items(), key=lambda w: w[1], reverse=True) 
+
+# Print the top 5 words across all documents alongside the count
+for word_id, word_count in sorted_word_count[:5]:
+    print(dictionary.get(word_id), word_count)
